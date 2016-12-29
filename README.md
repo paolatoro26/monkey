55509f5f6f894a5d26d48acab3e69d0aea26640f
paola <paolatorobejar@gmail.com>
2016-12-29 08:00:45 -0500

    index completo

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # monkey
+
+Inicio: Monkey
+
+## Ingreso de datos
+- bsmile
+- asmile
+- resultado
+
+##Proceso
+- obtener asmile
+- obtener bsmile
+- si bsmile es igual a asmile entonces estamos en problemas
+ 	mostrar 1
+- si no, No estamos en problemas
+	mostrar 0
+
+*Fin*
---
+++ js/main.js
@@ -0,0 +1,39 @@
+function monoA()
+{
+	var img=document.getElementById("img_mono_a");
+	var checkB=document.getElementById("mono_a");
+
+	if(checkB.checked)
+		img.innerHTML= '<img src="../img/feliz.jpg">';
+	else
+		img.innerHTML= '<img src="../img/triste.jpg">';
+	
+}
+
+
+function monoB()
+{
+	var img=document.getElementById("img_mono_b");
+	var checkB=document.getElementById("mono_b");
+
+	if(checkB.checked)
+		img.innerHTML= '<img src="../img/feliz.jpg">';
+	else
+		img.innerHTML= '<img src="../img/triste.jpg">';
+
+}
+
+function evaluar()
+{
+	var asmile = document.getElementById("mono_a").checked;
+	var bsmile = document.getElementById("mono_b").checked;
+	var salida = document.getElementById("salida");
+
+	if(asmile == bsmile)
+	{
+		salida.innerHTML = "1";
+	}else{		
+		salida.innerHTML = "0";
+	}
+}
+
