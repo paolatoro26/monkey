1afcfdbf29b51885670fb8577d07150e57fb3442
paola <paolatorobejar@gmail.com>
2017-01-13 16:24:44 -0500

    html mejorado

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ function monoA()
 	var checkA=document.getElementById("mono_a");
 
 	if(checkA.checked){
-		img.innerHTML= "<img src='../img/feliz.jpg'>";
+		img.innerHTML= "<img src='https://s-media-cache-ak0.pinimg.com/564x/0d/74/d1/0d74d1cfb2dd9172b8a606e508d5cab9.jpg'>";
 	}else{
-		img.innerHTML= "<img src='../img/triste.jpg'>";
+		img.innerHTML= "<img src='https://cdn.pixabay.com/photo/2014/04/02/10/17/monkey-303353_960_720.png'>";
 	}
 }
 
@@ -17,9 +17,9 @@ function monoB()
 	var checkB=document.getElementById("mono_b");
 
 	if(checkB.checked){
-		img.innerHTML= "<img src='../img/feliz.jpg'>";
+		img.innerHTML= "<img src='https://s-media-cache-ak0.pinimg.com/564x/0d/74/d1/0d74d1cfb2dd9172b8a606e508d5cab9.jpg'>";
 	}else{
-		img.innerHTML= "<img src='../img/triste.jpg'>";
+		img.innerHTML= "<img src='https://cdn.pixabay.com/photo/2014/04/02/10/17/monkey-303353_960_720.png'>";
 	}	
 }
 
